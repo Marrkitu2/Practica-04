0c94a5d5015904ce47f0c241fc7681b69facbccb
Marcos Lopez Medina <166430537+Marrkitu2@users.noreply.github.com>
2024-10-20 16:13:07 +0200

    Creamos la estructura principal del proyecto y la conexion con la base de datos.

---
+++ model/db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Canvia segons la teva configuració
+$password = ""; // Canvia segons la teva configuració
+$dbname = "pt4_Marcos_Lopez"; // Canvia segons la teva base de dades
+
+// Crear connexió
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la connexió
+if ($conn->connect_error) { // Canvia $mysqli per $conn
+    die("Connexió fallida: " . $conn->connect_error);
+}
+?>
